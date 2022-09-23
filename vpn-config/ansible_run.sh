c8ec0ed96c099beb1df574a5aa9f9501a7709d48
IvanRezv@ <ansible-mast@ubuntu.com>
2022-09-23 11:49:31 +0300

    fixes

--- vpn-config/ansible_run.sh
+++ vpn-config/ansible_run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-function GIT_CHECKOUT () {
+function ANSIBLE_RUN () {
   ansible-playbook -i inventory.ini --ask-vault-pass --extra-vars 'private_key=private_key_ivan public_key=public_key_ivan' playbook-client.yaml
 }
+
+ANSIBLE_RUN
